3475e9da2dfcf936e59cd49c05d6b7d7b8214401
Justin Napolitano <napolitano.just@gmail.com>
2025-11-23 04:14:27 -0500

    chore: auto-generate README

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,31 @@
-gcloud run jobs deploy loc-scraper --image us-west2-docker.pkg.dev/smart-axis-421517/supreme-court-scraper/supreme-court-scraper-image:dev --region us-west2
+#!/bin/bash
 
+# Set variables
+IMAGE_NAME="loc-flattener-image"
+PROJECT_ID="strategic-kite-431518-d87"
+REGION="us-west2"
+ARTIFACT_REGISTRY="us-west2-docker.pkg.dev/$PROJECT_ID/$IMAGE_NAME"
+
+# Build the Docker image locally
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+# Tag the Docker image
+echo "Tagging Docker image..."
+docker tag $IMAGE_NAME $ARTIFACT_REGISTRY
+
+# Authenticate Docker to Google Artifact Registry
+echo "Authenticating Docker with Google Artifact Registry..."
+gcloud auth configure-docker $REGION-docker.pkg.dev
+
+# Push the Docker image to Google Artifact Registry
+echo "Pushing Docker image to Google Artifact Registry..."
+docker push $ARTIFACT_REGISTRY
+
+# Deploy to Google Cloud Run (optional)
+echo "Deploying to Google Cloud Run..."
+gcloud run deploy loc-flattener-service \
+  --image $ARTIFACT_REGISTRY \
+  --region $REGION
+
+echo "Deployment complete."
